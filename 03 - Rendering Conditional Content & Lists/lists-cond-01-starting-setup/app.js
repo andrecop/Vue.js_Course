2b474a76ea9f1913a298b6a89e305cfa1ecbcd6f
andrecop <coppetta.andrea@gmail.com>
2022-02-07 18:23:12 +0100

    Project 10 - Exercise Rendering Conditional Content & Lists

--- 03 - Rendering Conditional Content & Lists/lists-cond-01-starting-setup/app.js
+++ 03 - Rendering Conditional Content & Lists/lists-cond-01-starting-setup/app.js
@@ -1,6 +1,21 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      enteredGoal: '',
+      goals: [],
+      objects: {name: 'Andrea', age: 26}
+    };
+  },
+  methods: {
+    addGoal(){
+      if(this.enteredGoal != ''){
+        this.goals.push(this.enteredGoal);
+        this.enteredGoal = '';
+      }
+    },
+    removeGoal(idx){
+      this.goals.splice(idx, 1);
+    }
   },
 });
 
