2dbc63da991ae024c15339513f3876400dfa5777
andrecop <coppetta.andrea@gmail.com>
2022-02-07 16:20:18 +0100

    Project 6 - Exercise Two-Way Binding

--- 02 - Basics & Core Concepts (DOM Interaction)/basics-05-using-the-native-event-object/app.js
+++ 02 - Basics & Core Concepts (DOM Interaction)/basics-05-using-the-native-event-object/app.js
@@ -2,19 +2,61 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname: ''
     };
   },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0
+        }, 2000);
+      }
+    }
+    // name(value) {
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }
+    //   this.fullname = value + " " + this.lastName;
+    // },
+    // lastName(value) {
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }
+    //   this.fullname = this.name + " " + value;
+    // }
+  },
+  computed: {
+    fullname() {
+      if(this.name === '' && this.lastName === ''){
+        return '';
+      }
+      return this.name + " " + this.lastName;
+    }
+  },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    outputFullname() {
+      if(this.name === ''){
+        return '';
+      }
+      return this.name + " " + "Cop";
     },
+    // setName(event, lastName) {
+    //   this.name = event.target.value;
+    // },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
+      this.lastName = '';
     }
   }
 });
