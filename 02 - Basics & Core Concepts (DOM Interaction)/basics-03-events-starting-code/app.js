e2a5941586a2d1fb729fa74be4bf2f94cbc0c109
andrecop <coppetta.andrea@gmail.com>
2022-02-07 15:10:11 +0100

    Project 4 - Exercise Event Binding

--- 02 - Basics & Core Concepts (DOM Interaction)/basics-03-events-starting-code/app.js
+++ 02 - Basics & Core Concepts (DOM Interaction)/basics-03-events-starting-code/app.js
@@ -2,7 +2,26 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: ''
     };
+  },
+  methods: {
+    add(number) {
+      this.counter += number
+    },
+    subtract(number) {
+      this.counter -= number
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      alert('Submitted!')
+    }
   },
 });
 
