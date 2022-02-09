d1a74ace82daae1c33084f918f9954b6fc58060d
andrecop <coppetta.andrea@gmail.com>
2022-02-10 00:12:35 +0100

    Project 17 - Exercise Props & Custom Events

---
+++ 08 - Props (Parent - Child Communication)/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <active-user
+      :username="user.username"
+      :age="user.age"
+      ></active-user>
+  </section>
+  <user-data
+    @set-data="setUserData"
+  ></user-data>
+</template>
+
+<script>
+import ActiveUser from './components/ActiveUser.vue'
+  export default {
+  components: { ActiveUser },
+    data() {
+      return {
+        user: {
+          username: 'pippo',
+          age: 23
+        }
+      }
+    },
+    methods: {
+      setUserData(name, age) {
+        this.user = {
+          username: name,
+          age: +age
+        }
+      }
+    },
+  }
+</script>
+
+<style>
+html {
+  font-family: "Jost", sans-serif;
+}
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+</style>
