2044a96ed6b7c46126d77f2714cbc6e5b153e7d0
andrecop <coppetta.andrea@gmail.com>
2022-02-24 15:47:01 +0100

    setup() - refs, reactive, toRefs & functions

--- 19 - The Composition API (Replacing the Options API)/composition-01-starting-setup/src/App.vue
+++ 19 - The Composition API (Replacing the Options API)/composition-01-starting-setup/src/App.vue
@@ -1,16 +1,110 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ user.name }}</h2>
+    <h2>{{ user.age }}</h2>
+    <!-- <h2>{{ userName }}</h2>
+    <h2>{{ age }}</h2> -->
+
+    <!-- <button @click="user.age = 32">Change Age</button> -->
+    <button @click="setAge">Change Age</button>
+    <!-- {{ setAge() }} -->
   </section>
 </template>
 
 <script>
+// import { ref } from 'vue';
+// import { reactive, toRefs } from 'vue';
+import { reactive } from 'vue';
+// import { reactive, isReactive, isRef } from 'vue';
+
 export default {
-  data() {
+
+  // ------- OPTION 1
+  // setup() {
+  //   const uName = ref('Maximilian');
+  //   const uAge = ref(31);
+
+  //   setTimeout(function() {
+  //     uName.value = 'Max';
+  //     uAge.value = 32;
+  //   }, 2000)
+
+  //   return {
+  //     userName: user.value.name,
+  //     age: user.value.age,
+  //   };
+  // }
+  
+  // ------- OPTION 2
+  // setup() {
+  //   const user = ref({
+  //     name: 'Maximilian',
+  //     age: 31,
+  //   });
+
+  //  console.log(isReactive(user));
+  //  console.log(isRef(user));
+
+  //   setTimeout(function() {
+  //     user.value.name = 'Max';
+  //     user.value.age = 32;
+  //   }, 2000)
+
+  //   return {
+  //     user: user
+  //   };
+  // }
+  
+  // ------- OPTION 3
+  // setup() {
+  //   const user = reactive({
+  //     name: 'Maximilian',
+  //     age: 31,
+  //   });
+
+  //   setTimeout(function() {
+  //     user.name = 'Max';
+  //     user.age = 32;
+  //   }, 2000)
+
+  //   const userRefs = toRefs(user);
+
+  //   return {
+  //     user: user,
+  //     userName: userRefs.name,
+  //     age: userRefs.age,
+  //   };
+  // }
+  
+  // ------- OPTION 3 - clean
+  setup() {
+    const user = reactive({
+      name: 'Maximilian',
+      age: 31,
+    });
+
+    function setNewAge() {
+      user.age = 32;
+    }
+
     return {
-      userName: 'Maximilian',
+      user: user,
+      setAge: setNewAge
     };
-  },
+  }
+
+  // ------- OPTION OLD
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //     age: 31,
+  //   };
+  // },
+  // methods: {
+  //   setNewAge() {
+  //     this.age = 31;
+  //   }
+  // },
 };
 </script>
 
